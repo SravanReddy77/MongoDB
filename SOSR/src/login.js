7e879544cb147a5c4036b6ab37e4bb09d2ae95eb
SravanReddy77 <sravanreddy.annadi7777@gmail.com>
2023-09-14 15:49:12 -0500

    API integration with UI
    API integration with UI

--- SOSR/src/login.js
+++ SOSR/src/login.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import "./css/login.css"
 import Homepage from "./homepage";
 import Register from "./register";
+import axios from "axios";
+
 
 function Login () {
-    function Submit  ()   {
-         alert("You are logged in!")
-    }
+    function handleSubmit  ()   {
+        const configuration = {
+            method: "post",
+            url: "http://localhost:4000/login",
+            data: {
+              email,
+              password,
+            },
+          };
+          axios(configuration)
+          .then((result) => {
+            setLogin(true);
+          })
+          .catch((error) => {
+            error = new Error();
+          });
+          }
+    
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
     return(
         <div className="body">
         <div className="mainclass">
         <h1 className="header">Welcome to New Castle</h1>
             <h1 className="header">Login Page</h1>
             
-            <input className = "input" type = "text" name = "Username" placeholder="Username"/><br /><br/>
-            <input className = "input" type = "password" name = "Password" placeholder="Password" /><br /><br/>
-           <a href = "./homepage"> <button className="button-5" onClick={Homepage} type = "submit"> Submit </button></a>
+            <input className = "input" type = "email" name = "email" value={email} placeholder="email" onChange ={(e) => setEmail(e.target.value)}/><br /><br/>
+            <input className = "input" type = "password" name = "Password" value = {password} placeholder="Password" onChange ={(e) => setPassword(e.target.value)} /><br /><br/>
+           <button className="button-5" type = "submit" onClick={(e) => handleSubmit(e)}> Login </button>
            <a href = "./register"> <button className="button-5" onClick={Register} type = "submit"> Register </button></a><br /><br/><br /><br/><br /><br/><br /><br/><br/>
+           {login ? (
+          <p className="text-success">You Are Logged in Successfully</p>
+        ) : (
+          <p className="text-danger">You Are Not Logged in</p>
+        )}
         </div>
         </div>
     )
--- SOSR/src/register.js
+++ SOSR/src/register.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Login from "./login";
 import { redirect } from "react-router-dom";
+import axios from "axios";
 
 function Register () {
 const [username, setUsername] = useState();
@@ -10,27 +11,40 @@ const [register, setRegister] = useState(false);
 const handleSubmit = (e) => {
     e.preventDefault();
     const configuration = {
-        method: "post",
-        url: "./",
-        data: {
-          email,
-          password,
-        },
-      };
+      method: "post",
+      url: "http://localhost:4000/register",
+      data: {
+        email,
+        password,
+        username,
+      },
+    };
+    axios(configuration)
+    .then((result) => {
+      setRegister(true);
+    })
+    .catch((error) => {
+      error = new Error();
+    });
+
   }
     return(
         <div className="body">
         <div className="mainclass">
             <h1 className="header">Register Page</h1>
             
-            <input className = "input" type = "text" name = "Username" placeholder="Username" onChange = {(e) => setUsername(e.target.value)}/><br /><br/>
-            <input className = "input" type = "text" name = "Email" placeholder="Email"onChange = {(e) => setEmail(e.target.value)}/><br /><br/>
-            <input className = "input" type = "password" name = "Password" placeholder="Password" onChange = {(e) => setPassword(e.target.value)}/><br /><br/>
+            <input className = "input" type = "text" name = "Username" placeholder="Username" value={username} onChange = {(e) => setUsername(e.target.value)}/><br /><br/>
+            <input className = "input" type = "text" name = "Email" placeholder="Email" value={email} onChange = {(e) => setEmail(e.target.value)}/><br /><br/>
+            <input className = "input" type = "password" name = "Password" placeholder="Password" value = {password} onChange = {(e) => setPassword(e.target.value)}/><br /><br/>
            <a href = "./"> <button className="button-5" onClick={(e) => handleSubmit(e)} type = "submit"> Submit </button></a>
-
+           {register ? (
+          <p className="text-success">You Are Registered Successfully</p>
+        ) : (
+          <p className="text-danger">You Are Not Registered</p>
+        )}
         </div>
         </div>
-    )
+    ) 
 }
 
 
